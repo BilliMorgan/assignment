c8852651d0150dc8c46fce610ca4c3bdc7f26097
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-09-02 22:29:11 -0400

    task number 5

--- src/App.js
+++ src/App.js
@@ -1,36 +1,69 @@
 import React, { Component } from "react";
 import "./App.css";
 import ValidationComponent from "./ValidationComponent/ValidationComponent";
+import CharComponent from "./CharComponent/CharComponent";
 
 class App extends Component {
+ 
   state = {
-    print: [{ output: "Output will be here" }],
+    print: [
+      { output: "Output will be here" },
+      
+    ],
     showOutput: false,
   };
 
   input = (event) => {
     let inputWord = event.target.value.length;
+    //let letters = "test";
     let outputWord = "";
     if(inputWord < 5){
       outputWord = "Text too short"
     } else {
       outputWord = "Text long enough"
     }
+  
     this.setState({
-      print: [{ output: outputWord }],
-      showOutput: true
+      print: [
+        { output: outputWord }, 
+        { letters: "test2" }
+      ],
+
+      showOutput: true,
     });
   };
+
   render() {
+    const style = {
+      display: 'inline-block',
+      padding: '16px',
+      textAlign: 'center',
+      margin: '16px',
+      border: '1px solid black'
+    }
     let output = null;
+
     if (this.state.showOutput) {
-      output = this.state.print[0].output};
+      output = (
+      <div>
+        <ValidationComponent wordLength={this.state.print[0].output} />
+      </div>)
+      }
+      
+        
       return (
         <div>
           <h1> Hello World</h1>
           <input type="text" onChange={this.input} />
+          {output}
 
-          <ValidationComponent wordLength={output} />
+          <div style={style}>
+            <CharComponent char={this.state.print[1]} />
+
+            {/* {this.state.letters.map((letter) => {
+              return <CharComponent char={this.letter} />;
+            })} */}
+          </div>
         </div>
       );
     
---
+++ src/CharComponent/CharComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CharComponent = (props) => {
+  return (
+    <div>
+      <p>Hello Hello</p>
+      <p>Here input:{props.char}</p>
+    </div>
+  );
+  
+}
+
+export default CharComponent
